a2d4a05d57158ffab9261a22f7e12a50ec85dae9
Kavishna Thathsara <kavishna@Kavishnas-MacBook-Pro.local>
2024-01-27 21:37:39 +0530

    day11 - finally completed

--- app.js
+++ app.js
@@ -10,36 +10,47 @@ const generateButton = document.querySelector(".generate");
 const scoreElements = document.querySelectorAll(".score");
 const strengthText = document.querySelector(".strength-text");
 
-const Chars = {
-  Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
-  Lowers: "qwertyuiopasdfghjklzxcvbnm",
-  Numbers: "1234567890",
-  Symbols: "!@#$%^&*",
-};
-
 const Allowed = {};
+// this function checks if what checkbox is checked and returns appropriate combination
+function updateAllowed() {
+  for (var key in Allowed) {
+    delete Allowed[key];
+  }
 
+  if (uppercaseCheckbox.checked) {
+    Allowed.Uppers = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  }
+  if (lowercaseCheckbox.checked) {
+    Allowed.Lowers = "qwertyuiopasdfghjklzxcvbnm";
+  }
+  if (numbersCheckbox.checked) {
+    Allowed.Numbers = "1234567890";
+  }
+  if (symbolsCheckbox.checked) {
+    Allowed.Symbols = "!@#$%^&*";
+  }
+}
+
+//based on the input values (uppercase, lowercase,numbers or symbols) function returns random numbers characters from provided string
 const getRandomCharFromString = (str) => {
-  console.log(str);
-  str.charAt(Math.floor(crypto.randomInt(0, str.length)));
+  const randomIndex = Math.floor(
+    (crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1)) *
+      str.length
+  );
+  return str.charAt(randomIndex);
 };
 
+//function will return a password that contains atleast one of selected characters
 const generatePassword = (length = lengthInput.value) => {
   let pwd = "";
-  pwd += getRandomCharFromString(Allowed.Uppers); // pwd will have at least one upper
-  pwd += getRandomCharFromString(Allowed.Lowers); // pwd will have at least one lower
-  pwd += getRandomCharFromString(Allowed.Numbers); // pwd will have at least one number
-  pwd += getRandomCharFromString(Allowed.Symbols); // pwd will have at least one symbol
+  if (uppercaseCheckbox.checked) pwd += getRandomCharFromString(Allowed.Uppers);
+  if (lowercaseCheckbox.checked) pwd += getRandomCharFromString(Allowed.Lowers);
+  if (numbersCheckbox.checked) pwd += getRandomCharFromString(Allowed.Numbers);
+  if (symbolsCheckbox.checked) pwd += getRandomCharFromString(Allowed.Symbols);
   for (let i = pwd.length; i < length; i++)
-    pwd += getRandomCharFromString(Object.values(Allowed).join("")); // fill the rest of the pwd with random characters
+    pwd += getRandomCharFromString(Object.values(Allowed).join(""));
   return pwd;
 };
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (pwText) {
-    pwText.classList.add("dimm");
-  }
-});
 
 function getPasswordStrength(password) {
   var lengthScore = password.length >= 8 ? 1 : 0;
@@ -54,8 +65,6 @@ function getPasswordStrength(password) {
     lowercaseScore +
     digitScore +
     specialCharScore;
-
-  console.log(totalScore);
 
   switch (totalScore) {
     case 1:
@@ -73,9 +82,15 @@ function getPasswordStrength(password) {
   }
 }
 
-uppercaseCheckbox.addEventListener("checked", function () {});
+//change the color of password text to lower color h2.pw-text
+document.addEventListener("DOMContentLoaded", function () {
+  if (pwText) {
+    pwText.classList.add("dimm");
+  }
+});
 
 generateButton.addEventListener("click", function () {
+  updateAllowed(); //check the combination and update Allowed object
   let password = generatePassword();
   pwText.innerHTML = password;
   const passwordStrength = getPasswordStrength(password);
@@ -83,6 +98,7 @@ generateButton.addEventListener("click", function () {
 
   pwText.classList.remove("dimm");
 
+  //remove all styles from strength meter
   for (var i = 0; i < scoreElements.length; i++) {
     scoreElements[i].classList.remove(
       "weak",
@@ -93,6 +109,7 @@ generateButton.addEventListener("click", function () {
     );
   }
 
+  // and add the new styles based on the password strength
   switch (passwordStrength) {
     case "Weak":
       for (var i = 0; i < 1 && i < scoreElements.length; i++) {
@@ -124,15 +141,17 @@ generateButton.addEventListener("click", function () {
   }
 });
 
-lengthInput.addEventListener("input", function (event) {
-  lengthIndicator.innerHTML = event.target.value;
-  console.log(event.target.value);
-});
-
+//set the initial value of password length when loading
 document.addEventListener("DOMContentLoaded", function () {
   lengthIndicator.innerHTML = lengthInput.value;
 });
 
+//shows the selected  password length
+lengthInput.addEventListener("input", function (event) {
+  lengthIndicator.innerHTML = event.target.value;
+});
+
+//when clicking on the copy button this will add little animation (scale up) for 500ms
 copyButton.addEventListener("click", () => {
   navigator.clipboard.writeText(pwText.textContent);
 
@@ -143,30 +162,7 @@ copyButton.addEventListener("click", () => {
   }, 500);
 });
 
-function updateAllowed(charType, isChecked) {
-  if (isChecked) {
-    Allowed[charType] = Chars[charType];
-  } else {
-    delete Allowed[charType];
-  }
-}
-
-uppercaseCheckbox.addEventListener("change", function () {
-  updateAllowed("Uppers", this.checked);
-});
-
-lowercaseCheckbox.addEventListener("change", function () {
-  updateAllowed("Lowers", this.checked);
-});
-
-numbersCheckbox.addEventListener("change", function () {
-  updateAllowed("Numbers", this.checked);
-});
-
-symbolsCheckbox.addEventListener("change", function () {
-  updateAllowed("Symbols", this.checked);
-});
-
+//this code is belongs to input[type="range"] amd ot will update the color befor and after the slider-thumb
 const slider = lengthInput;
 const min = slider.min;
 const max = slider.max;
